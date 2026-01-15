17e102772f87606e4f506259f54ad79ab8eb3ca1
Clare Ming <cming@wikimedia.org>
2026-01-15 13:15:11 -0700

    Update experiment code for JS, PHP SDKs testing of TK
    - Repurpose JS experiment to test TK JS SDK
    - Add simple PHP experiment code to test TK PHP SDK
    - Take opportunity to remove PageBeacon code, test
    
    Bug: T414528
    Bug: T414530
    Change-Id: I2153037fad09507fed15b75faf7f69c627355db2
    (cherry picked from commit 18218edcc19ee2a91f428596b8914ec370b95bd7)

--- modules/ext.testKitchen/Experiments.js
+++ modules/ext.testKitchen/Experiments.js
@@ -1,11 +1,11 @@
 /**
  * A simple experiment-specific instrument that sends a "testKitchen-loaded" event if the current user is
- * enrolled in the "test-kitchen-mw-module-loaded-v2" experiment and assigned to the treatment group.
+ * enrolled in the "test-kitchen-mw-module-loaded" experiment and assigned to the treatment group.
  *
- * See https://phabricator.wikimedia.org/T403507 for more context.
+ * See https://phabricator.wikimedia.org/T414528 for more context.
  */
 
-const experiment = mw.testKitchen.getExperiment( 'test-kitchen-mw-module-loaded-v2' );
+const experiment = mw.testKitchen.getExperiment( 'test-kitchen-mw-module-loaded' );
 
 if ( experiment.isAssignedGroup( 'control', 'treatment' ) ) {
 	experiment.send(
